4d78e7b7c5d418bd27971af58245d3bdac7a5883
cjm771 <chrisishere@gmail.com>
2014-08-02 14:41:04 -0400

    update migration files

--- db/migrate/20140801222104_change_role_active_to_integer.rb
+++ db/migrate/20140801222104_change_role_active_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeRoleActiveToInteger < ActiveRecord::Migration
   def change
-  	change_column :roles, :active,  :integer
+  	change_column :roles, :active,  'integer USING CAST(active AS integer)'
   end
 end
